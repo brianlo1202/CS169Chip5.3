e005e4d2f65a14eadf4dc3263ba4461584943fb6
Brian Lo <blo8@berkeley.edu>
2020-10-28 09:35:02 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ActiveRecord::Base
   def Movie.all_ratings
     return ['G','PG','PG-13','R']
   end
+  
+  def Movie.with_ratings(ratings)
+    if ratings.empty?
+      return self.all 
+    else
+      return where(rating: ratings)
+    end
+  end
 end
